358a099b0c9b2cbd997d66da633764eef200d588
irfanfadhullah <57250324+irfanfadhullah@users.noreply.github.com>
2019-12-13 22:14:24 +0700

    Create buat_tavel.sql

---
+++ buat_tavel.sql
@@ -0,0 +1,8 @@
+create table [dbo].[Registration](
+    id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
+    namabarang VARCHAR(50),
+    jenis VARCHAR(50),
+    letak VARCHAR(50),
+    pemilik VARCHAR(50),
+    date DATE
+);
